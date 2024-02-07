75444c3e69064ed8ddcc3228fe6f5dc2efd25184
David Glaeseman <backgroundcolor@gmail.com>
2024-02-06 17:32:01 -0700

    feat: test

--- src/s3Drive.ts
+++ src/s3Drive.ts
@@ -1,32 +1,70 @@
-import {PutObjectCommand, PutObjectCommandOutput, S3Client} from "@aws-sdk/client-s3";
-
+import {
+    DeleteObjectCommand,
+    GetObjectCommand,
+    PutObjectCommand,
+    PutObjectCommandOutput,
+    S3Client
+} from "@aws-sdk/client-s3";
+import 'dotenv/config'
 export const s3Drive = () => {
-    const Bucket = 'bucket-name-goes-here'
-    const configuration = {
-        forcePathStyle: false, // Configures to use subdomain/virtual calling format.
-        endpoint: "https://sfo2.digitaloceanspaces.com",
-        region: "us-west-1",
+    const Bucket = process.env.S3_BUCKET
+    const client = new S3Client({
+        forcePathStyle: false,
+        endpoint: process.env.S3_ENDPOINT,
+        region: process.env.S3_REGION,
         credentials: {
-            accessKeyId: process.env.SPACES_KEY,
-            secretAccessKey: process.env.SPACES_SECRET
+            accessKeyId: process.env.S3_KEY,
+            secretAccessKey: process.env.S3_SECRET
         }
-    }
-    console.log({configuration})
-    const client = new S3Client({});
-    const put = async (filePath: string, body: any): Promise<PutObjectCommandOutput> => {
+    });
+    const put = async (filePath: string, body: any) => {
         try {
-            return await client.send(new PutObjectCommand({
+            const response = await client.send(new PutObjectCommand({
                 Bucket,
                 Key: filePath,
                 Body: body,
             }));
+            return response.$metadata
         } catch (err) {
-            console.error(err);
             return Promise.reject(err)
         }
     }
+    const get = async (filePath: string, type?: 'string' | 'byteArray' | 'webStream') => {
+        const command = new GetObjectCommand({
+            Bucket,
+            Key: filePath,
+        });
+
+        try {
+            const response = await client.send(command);
+            if(type === 'webStream'){
+                return  response.Body.transformToWebStream();
+            } else if(type === 'byteArray'){
+                return await response.Body.transformToByteArray()
+            }
+            return await response.Body.transformToString();
+        } catch (err) {
+            return Promise.reject(err)
+        }
+    }
+    const remove = async (filePath: string) => {
+        const command = new DeleteObjectCommand({
+            Bucket,
+            Key: filePath,
+        });
+
+        try {
+            const response = await client.send(command);
+            return response.$metadata
+        } catch (err) {
+            return Promise.reject(err)
+        }
+
+    }
 
     return {
-        put
+        get,
+        put,
+        remove
     }
 }
--- test/test.ts
+++
@@ -1,4 +0,0 @@
-import {s3Drive} from "../dist";
-
-const drive = s3Drive()
-console.log({drive})
